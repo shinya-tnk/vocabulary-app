5b96d7ef7309aca2fffd203ff15210838cb5f1cd
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-02-07 14:05:20 +0900

    rspec, factory_bot, faker, rubocop導入

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,7 +12,9 @@ module VocabularyApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
