2ec4c773fb6155365dcc5b870fe8f46c38025bf7
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-02-07 14:32:05 +0900

    vocabularyモデルバリデーション設定

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,6 +1,11 @@
-# frozen_string_literal: true
-
 class Vocabulary < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :word, format: {
+      with: /\A^[a-zA-Z]+$\z/
+    }
+    validates :description1
   end
 
 end
