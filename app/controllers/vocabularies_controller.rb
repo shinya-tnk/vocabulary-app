500ee3ead80ca74e7dada140b9531c881067af9b
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-02-07 14:35:30 +0900

    単語新規登録画面実装

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -1,8 +1,26 @@
-# frozen_string_literal: true
-
 class VocabulariesController < ApplicationController
   def index
-    @vocabularies = Vocabulary.all
+    @vocabularies = Vocabulary.where(user_id: [current_user.id, 1]).sample(20) if user_signed_in?
   end
 
+  def new
+    @vocabulary = Vocabulary.new
+  end
+
+  def create
+    @vocabulary = Vocabulary.new(vocabulary_params)
+    if @vocabulary.save
+      flash[:alert] = '単語を登録しました！'
+      redirect_to action: :new
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def vocabulary_params
+    params.require(:vocabulary).permit(:word, :description1, :description2, :description3, :description4, :description5).merge(user_id: current_user.id)
+  end
 end
