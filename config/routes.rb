37192281d4754b61c9a1322f971126ccf4b79467
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-02-07 13:39:54 +0900

    トップページCSS編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'
+  }
 
   root to: 'vocabularies#index'
-  resources :vocabularies
+  resources :vocabularies do
+    collection do
+      get 'search'
+    end
+  end
+  resources :users, only: :show
 end
