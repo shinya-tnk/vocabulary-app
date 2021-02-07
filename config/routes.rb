ea7871ca5f3c4e1289b9e32a6fb8f49d38eca844
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-02-07 13:51:39 +0900

    deviseインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'
-  }
-
+  devise_for :users
   root to: 'vocabularies#index'
-  resources :vocabularies do
-    collection do
-      get 'search'
-    end
-  end
-  resources :users, only: :show
+  resources :vocabularies
 end
